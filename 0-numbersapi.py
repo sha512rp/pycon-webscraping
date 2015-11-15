4789aefee13841dace7e8251ea1fae38a4516847
Tomas Krizek <tomas.krizek@mailbox.org>
2015-11-15 10:33:53 +0100

    numbers api

---
+++ 0-numbersapi.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def main():
+    params = {
+        'json': '',
+
+    }
+    try:
+        page = requests.get('http://numbersapi.com/42', params=params)
+    except requests.ConnectionError:
+        print("Could not establish connection.")
+        exit()
+    print(page.json()['text'])
+
+if __name__ == '__main__':
+    main()
